fd127cfd0b0e26453a85b198676779041002405c
Evan Scarborough <evanesce49@gmail.com>
2019-05-22 19:38:13 -0500

    Most Plotting Works
    Most of the actual plotting functionality works

--- assets/editor.js
+++ assets/editor.js
@@ -1,8 +1,9 @@
 
 
 
-
-var myCodeMirror = CodeMirror(document.getElementById('editor'), {
+// setting up the codemirror formatting on the editor.
+// initialize it with some data too.
+var jsonEditor = CodeMirror(document.getElementById('editor'), {
   value: "{type: 'start', timestamp: 1519780251293, select: ['min_response_time', 'max_response_time'], group: ['os', 'browser']}\n\
 {type: 'span', timestamp: 1519780251293, begin: 1519780251293, end: 1519780260201}\n\
 {type: 'data', timestamp: 1519780251000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}\n\
@@ -11,3 +12,289 @@ var myCodeMirror = CodeMirror(document.getElementById('editor'), {
   lineNumbers: true,
   json: true
 });
+
+// some handy selectors to cache!
+var $plot = $('#plotArea');
+var $bottomPanel = $('.panel-bottom');
+var $topPanel = $('.panel-top');
+var $chartContainer = $('.chart-container');
+
+// make the sections resizable
+$topPanel.resizable({
+  handleSelector: ".splitter-horizontal",
+  resizeWidth: false
+});
+
+// some variables we will use
+var span_min = 0;
+var span_max = 0;
+var started = false;
+var groups = [];
+var select = [];
+var warnings = "";
+function warn(w){warnings=warnings+w+'\n';}
+var data = {};
+
+function formatRawJSON(rawText){
+  // first of all, put "" around each word that doesn't have ""
+  var formattedText = rawText.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ');
+  // then replace all '' with "" (NOTE: this could be better, it would break any
+  // actual ' like if you wanted a plot called "Evan's Data" or whatever)
+  formattedText = formattedText.replace(/'/g,'"');
+  // put , between each of the events
+  formattedText = formattedText.replace(/}/g,'},');
+  // but delete the last ,
+  formattedText = formattedText.substring(0, formattedText.length - 1);
+  // then put them all in a json array
+  formattedText = '{"events":[' + formattedText.replace(/'/g,'"') + ']}';
+  return formattedText;
+}
+
+// this function is for when the generate button is clicked.
+// it will read in the json and update the plot accordingly
+function updatePlot(){
+  var rawText = jsonEditor.getValue();
+  var formattedText = formatRawJSON(rawText);
+
+  // now we can parse it!
+  warnings = "";
+  started = false;
+  var eventType, eventTime;
+
+  try{
+    var json = JSON.parse(formattedText);
+    // read the events line by line
+    for(var i = 0; i < json.events.length; i++){
+      eventType = json.events[i].type;
+      eventTime = json.events[i].timestamp;
+
+      // here are all of the actual events!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      if(eventType == 'start'){startEvent(json.events[i]);}
+      else if(eventType == 'span'){spanEvent(json.events[i]);}
+      else if(eventType == 'data'){
+        if(typeof eventTime == 'undefined'){throw('Missing timestamp from data! (event '+(i+1)+')');}
+        dataEvent(json.events[i]);
+      }
+      else if(eventType == 'stop'){stopEvent(json.events[i]);}
+      else{
+        warn('Unknown event type: ' + eventType + ' (event ' + (i+1) + ')');
+      }
+    }
+
+    if(warnings.length>0){alert("Warnings:\n"+warnings);warnings="";}
+    replotData();
+  }
+  catch (e){
+    var errorMsg = 'Bad JSON found :(\n';
+    errorMsg = errorMsg + e.toString();
+    alert(errorMsg);
+  }
+}
+
+function startEvent(event){
+  // NOTE: I'm not sure how I should handle when there are two start events...
+  // this would probably be an error, but I'm going to be liberal and
+  // allow two start events. They just build off of each other.
+  if(started == false){
+    // clear the data and reset if stopped
+    data = {};
+    span_min = 0;
+    span_max = 0;
+    groups = [];
+    select = [];
+  }
+  started = true;
+  var i;
+  for(i=0; i<event.group.length; i++){
+    groups.push(event.group[i]);
+  }
+  for(i=0; i<event.select.length; i++){
+    select.push(event.select[i]);
+  }
+}
+
+function spanEvent(event){
+  if(started == false){return;}
+  if(typeof event.begin !== 'undefined'){span_min=event.begin;}
+  if(typeof event.end !== 'undefined'){span_min=event.end;}
+}
+
+function dataEvent(event){
+  if(started == false){warn('Data event before start of series.');return;}
+  var time = event.timestamp;
+  // figure out what group this data belongs to.
+  var group = [];
+  for(var i=0; i < groups.length; i++){
+    var g = event[groups[i]];
+    if(typeof g != 'undefined'){group.push(event[groups[i]]);}
+    else{group.push('');}
+  }
+  // now add data for each series
+  var value;
+  for(i=0; i < select.length; i++){
+    value = event[select[i]];
+    if(typeof value != 'undefined'){
+      // this event has data for select[i]
+      var tempGroup = group.slice();
+      addData(data,tempGroup,select[i],time,value);
+    }
+  }
+}
+
+function addData(target,group,series,time,value){
+  if(group.length > 0){
+    var groupOn = group[0];
+    if(typeof target[groupOn] == 'undefined'){
+      target[groupOn] = {};
+    }
+    group.shift();
+    addData(target[groupOn],group,series,time,value);
+  }
+  else{
+    if(typeof target[series] == 'undefined'){target[series]=[];}
+    var i;
+    for(i = 0; i < target[series].length; i++){
+      if(target[series][i].time == time){target[series].splice(i,1);break;}
+      if(target[series][i].time > time){break;}
+    }
+    target[series].splice(i,0,{"time":time,"value":value});
+  }
+}
+
+
+function stopEvent(event){
+  started = false;
+}
+
+
+
+
+
+
+
+// creating the chart
+var chart = new Chart(document.getElementById('plotArea').getContext('2d'),{
+  "type":"scatter",
+  "data":{
+    "datasets":[
+      {
+      "label":"First Reading",
+      "data":[
+        {
+          x: -10,
+          y: 0
+        },{
+          x: 0,
+          y: 10
+        }, {
+          x: 10,
+          y: 5
+        }
+      ],
+      "showLine": true,
+      "fill":true,
+      "borderColor":"rgb(200, 0, 0)",
+      "lineTension":0
+      }
+    ]
+  },
+  options: {
+    maintainAspectRatio: false,
+    legend: {
+      position: 'right'
+    },
+    scales: {
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Y'
+        }
+      }],
+      xAxes: [{
+        scaleLabel: {
+          type: 'linear',
+          position: 'bottom',
+          display: true,
+          labelString: 'X'
+        }
+      }]
+    }
+  }
+});
+
+
+
+
+
+
+
+function replotData(){
+  // console.log(data);
+  chart.data.datasets = [];
+  recurseToDataset(data,0,"");
+  chart.update();
+}
+
+function recurseToDataset(target,depth,nameSoFar){
+  if(depth >= groups.length){
+    plotDataset(target,nameSoFar);
+    return;
+  }
+  for(var property in target) {
+    recurseToDataset(target[property],depth+1,nameSoFar + " " + property);
+  }
+}
+
+function plotDataset(target,name){
+  var series = "";
+  var point;
+  for(var i = 0; i < select.length; i++){
+    series = select[i];
+    if(typeof target[series] != 'undefined'){
+      var dataset = {"showLine": true,"fill":true,"lineTension":0};
+      dataset.label = name + " " + series;
+      dataset.borderColor = "hsl("+(Math.random()*360)+", 80%, 40%)";
+      dataset.data = [];
+      for(var j = 0; j < target[series].length; j++){
+        point = target[series][j];
+        dataset.data.push({x:point.time,y:point.value});
+      }
+      // console.log(dataset);
+      chart.data.datasets.push(dataset);
+    }
+  }
+}
+
+
+
+
+var lastHeight = 0;
+function resizeChart(){
+  // if the size has changed, resize the chart too.
+  // I had to do this with js. I got it to work with just css, but then
+  // realized that it only worked in Chrome :(
+  if(lastHeight != $bottomPanel.height()) {
+    lastHeight = $bottomPanel.height();
+    $chartContainer.height($bottomPanel.height());
+    $chartContainer.width($bottomPanel.width());
+  }
+}
+resizeChart();
+$(document).ready(function() {
+  $topPanel.height(150);
+  // Note, it might not be the best solution to have this on a timer, but I
+  // could not for the life of me get the jquery resize event to work!!
+  // I do not know why.
+  setInterval(function () {
+    resizeChart();
+  }, 60);
+});
+
+// vvv my failed attempts at a resize event. I tried these in doc ready too
+
+// $(".panel-top").on("resize", function(event,ui){
+//   console.log('hello?');
+// });
+// $(".panel-top").resize(function(){
+//   console.log('hello2?');
+// });
